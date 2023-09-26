5847e29e5565d44f5308fd95e676779bb3625ca6
ekramhossainfahad <ehfahad96@gmail.com>
2023-09-26 09:49:42 +0600

    declare variable

---
+++ variable.js
@@ -0,0 +1,15 @@
+// vary + able = variable
+
+// variable declaration: (five things you have to remember when you declare a variable:)
+var price = 45;
+var age = 34;
+var temperature = 32;
+var weight = 80;
+var money = 5000;
+var fruit = 450;
+var flower = 100;
+var book = 10;
+var shoes = 5;
+var pen = 10;
+var shirt = 15;
+var pant = 20;
