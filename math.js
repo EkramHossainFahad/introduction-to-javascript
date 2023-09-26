8e52c1a9db8487034c21a9eb675f63fd0b703c13
ekramhossainfahad <ehfahad96@gmail.com>
2023-09-26 09:31:41 +0600

    math.js file

---
+++ math.js
@@ -0,0 +1,33 @@
+/**
+ * 1. number: 55, 20, 63, -100 (any number however it is positive or negative);
+ * 
+ * 2. integer: 2, 10, 15, 89 (full number);
+ * 
+ * 3. float/decimal: 2.5, 13.2, 46.89;
+ * 
+ * 4. operation: addition, substraction, multiplication, division, remainder;
+ * 
+ * 5. percent: %;
+ * 
+ * 6. average:
+ * 
+ * 7. comparison: greater than (>), less than (<), equal (==), not equal (!=), equal or greater (>=), equal or less than (<=);
+ * 
+ * 8. unitary math:
+ * 
+ * 9. 45, 25, 68, 129, 2 - figure them out: max, min, average, sum;
+ * 
+ * 10. series:
+ * 
+ * 
+ * :::Extra:::
+ * 
+ * 1. prime number
+ * 
+ * 2. permutation
+ * 
+ * :::Always:::
+ * 
+ * if learn more if needed
+ * 
+ */
